1bbba722d2f4a3b78d38301ce37bf7362e382589
missdobis <missdobis@gmail.com>
2020-09-29 14:38:00 +0100

    project page

--- src/App.js
+++ src/App.js
@@ -13,14 +13,13 @@ function App() {
             <Navigation>
                 <Link to="/">Home</Link>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="My Portfolio">
             <Navigation>
                 <Link to="/resume">Resume</Link>
-                <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
             </Navigation>
--- src/Components/main.js
+++ src/Components/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import LandingPage from './landingpage';
-import AboutMe from './aboutme';
 import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
@@ -10,7 +9,6 @@ import Resume from './resume';
 const Main = () =>  
     <Switch>
         <Route exact path="/" component={LandingPage} />
-        <Route path="/aboutme" component={AboutMe} />
         <Route path="/contact" component={Contact} />
         <Route path="/projects" component={Projects} />
         <Route path="/resume" component={Resume} />
