c5665430c767e2a00e398b0a82fd6e3212391d23
missdobis <missdobis@gmail.com>
2020-09-30 09:14:40 +0100

    resume page

---
+++ src/Components/education.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
+
+
+class Education extends Component {
+    render() {
+        return (
+            <Grid>
+                <Cell col={4}></Cell>
+                <Cell col={8}></Cell>
+           </Grid>
+        )
+    }
+}
+
+export default Education;
--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import { Cell, Grid } from 'react-mdl';
+import Education from './education';
 
 
 class Resume extends Component {
     render() {
         return (
-            <div><h1>Resume Page</h1></div>
+            <div style={{ background: 'whitesmoke' }}>
+                <Grid>
+                    <Cell col={4}>
+                        <div style={{ textAlign: 'center' }}>
+                            <img
+                                src="./img/dobis.jpg"
+                                alt="avatar"
+                                style={{ height: "250px" }}
+                            />
+                        </div>
+                        <h2 style={{ paddingTop: '2em' }}>Adaobi Ezenwa</h2>
+                        <h4 style={{ color: '#141E30' }}>Programmer | Hardware Engineer</h4>
+                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <p>Adaobi is a Software Developer who is also a Hardware
+                        Engineer. Currently working as a Hardware Engineer at Hinckley
+                        Associates and interning as a FrontEnd developer at Dev_Careers
+                        with both companies based in Lagos, Nigeria.</p>
+                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                        <h5>Address</h5>
+                        <p>No 6 Ayodele street, Abule-Oja, Yaba, Lagos State, Nigeria.</p>
+                        <h5>Phone</h5>
+                        <p>+234 8104223355</p>
+                        <h5>Email</h5>
+                        <p>missdobis@gmail.com</p>
+                        <hr style={{ borderTop: '3px solid #833fb2', width: '50%' }} />
+                    </Cell>
+                    <Cell className="resume-right-col" col={8}>
+                        <h2>Education</h2>
+
+                        <Education />
+
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
